0b30c82ef12a64ca5d76533d94c5171d5e34c4e7
Abdoul <abdulmaliklawal12345@gmail.com>
2026-04-23 15:27:17 +0000

    added the test.js

---
+++ test/test.js
@@ -0,0 +1,24 @@
+// 23rd April 2024
+let assert = require("assert");
+let Phrase = require("../index.js");
+
+describe("Phrase", function(){
+    describe('#palindrome', function(){  // accessing methods with #
+        it("should return false for a non-palindrome", function(){
+            let nonPalindrome = new Phrase("apple");
+            assert(!nonPalindrome.palindrome());
+        });
+        it("should return true for a plain palindrome", function(){
+            let plainPalindrome = new Phrase("racecar");
+            assert(plainPalindrome.palindrome());
+        });
+        it("should return true for a mixed-case palindrome", function(){
+            let mixedCasePalindrome = new Phrase("Racecar");
+            assert(mixedCasePalindrome.palindrome());
+        });
+        it("should return true for a palindrome with punctuation", function(){
+            let punctuationPalindrome = new Phrase("Madam, I'm Adam");
+            assert(punctuationPalindrome.palindrome());
+        });
+    });
+});
